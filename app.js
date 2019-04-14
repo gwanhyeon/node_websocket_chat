45639b74324f6e23895110bd2231468ebb47a480
kgh940525 <kgh940525@gmail.com>
2019-04-15 03:20:50 +0900

    [client] Chat Room UI Update

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-const app = require('express')();
-const server = require('http').createServer(app);
+var app = require('express')();
+var server = require('http').createServer(app);
+// http server를 socket.io server로 upgrade한다
 var io = require('socket.io')(server);
 
-// index.html 렌더
-app.get('/',(req,res) =>{
-    res.sendFile(__dirname + '/index-room.html');
-})
-// namespace /chat 접속
-var chat = io.of('/chat').on('connection', (socket) =>{
-        socket.on('chat Message',(data) =>{
-        console.log('message from client : ', data);
-        var name = socket.name = data.name;
-        var room = socket.room = data.room;
-        // room join
-        socket.join(room);
-        // room에 조인되어 있는 클라이언트에게 메시지를 전송
-        chat.to(room).emit('chat message', data.msg);
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index-room.html');
+});
 
-    })
-})
+// namespace /chat에 접속한다.
+var chat = io.of('/chat').on('connection', function(socket) {
+  socket.on('chat message', function(data){
+    console.log('message from client: ', data);
 
-server.listen(3000, () =>{
-    console.log('Socket IO server Listening on Port 3000');
-})
+    var name = socket.name = data.name;
+    var room = socket.room = data.room;
+    var msg = socket.msg = data.mag;
+    
+    room = "1"
+    // room에 join한다
+    socket.join(room);
+    // room에 join되어 있는 클라이언트에게 메시지를 전송한다
+    chat.to(room).emit('chat message',"Nickname:" + data.name + "Massage:"+data.msg);
+    
+    
+    // chat.to(room).emit('chat message', data.name);
+    // chat.to(room).emit('chat message', data.room);
+    
+  });
+});
 
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
+});
