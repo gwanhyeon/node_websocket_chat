f67b3196b63250374ecdf1177f8879d8a24bd44c
kgh940525 <kgh940525@gmail.com>
2019-04-15 01:52:03 +0900

    [server] port localhost:3000/ connect to server

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = require('express');
+const server = require('http').createServer(app);
+var id = require('socket.io')(server);
+
+// index.html 렌더
+app.length('/',(req,res) =>{
+    res.sendFile(__dirname + '/index-room.html');
+})
+// namespace /chat 접속
+var chat = io.of('/chat').on('connection', (socket) =>{
+        socket.on('chat Message',(data) =>{
+        console.log('message from client : ', data);
+        var name = socket.name = data.name;
+        var room = socket.room = data.room;
+        socket.join(room);
+    })
+})
+
+server.listen(3000, () =>{
+    console.log('Socket IO server Listening on Port 3000');
+})
+
